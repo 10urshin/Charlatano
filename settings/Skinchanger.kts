4d8fb80e36d1fab0c15347b882d3035d711ae4fc
Austin72 <a.bc123itismeee@gmail.com>
2018-03-30 23:32:55 -0500

    Settings for Skinchanger
    (untested)

---
+++ settings/Skinchanger.kts
@@ -0,0 +1,156 @@
+/*
+ * Charlatano: Free and open-source (FOSS) cheat for CS:GO/CS:CO
+ * Copyright (C) 2017 - Thomas G. P. Nappo, Jonathan Beaudoin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.charlatano.settings
+
+/*
+ * For skin IDs: https://raw.githubusercontent.com/adamb70/CSGO-skin-ID-dumper/master/item_index.txt
+ * For statTrak, 0 = disabled, 12 = souvenir, any other number = statTrak value.
+ * For wear lower = less wear. 0F minimum - 1F maximum.
+ */
+
+ENABLE_SKINCHANGER = TRUE
+
+
+skin_AK47 = 180
+skin_AK47_STAT = 0
+skin_AK47_WEAR = 0.0001F
+
+skin_AUG = 455
+skin_AUG_STAT = 0
+skin_AUG_WEAR = 0.0001F
+
+skin_AWP = 344
+skin_AWP_STAT = 0
+skin_AWP_WEAR = 0.0001F
+
+skin_CZ75A = 687
+skin_CZ75A_STAT = 0
+skin_CZ75A_WEAR = 0.0001F
+
+skin_DESERT_EAGLE = 61
+skin_DESERT_EAGLE_STAT = 0
+skin_DESERT_EAGLE_WEAR = 0.0001F
+
+skin_FAMAS = 154
+skin_FAMAS_STAT = 0
+skin_FAMAS_WEAR = 0.0001F
+
+skin_FIVE_SEVEN = 427
+skin_FIVE_SEVEN_STAT = 0
+skin_FIVE_SEVEN_WEAR = 0.0001F
+
+skin_G3SG1 = 511
+skin_G3SG1_STAT = 0
+skin_G3SG1_WEAR = 0.0001F
+
+skin_GALIL = 379
+skin_GALIL_STAT = 0
+skin_GALIL_WEAR = 0.0001F
+
+skin_GLOCK = 353
+skin_GLOCK_STAT = 0
+skin_GLOCK_WEAR = 0.0001F
+
+skin_M249 = 496
+skin_M249_STAT = 0
+skin_M249_WEAR = 0.0001F
+
+skin_M4A1_SILENCER = 644
+skin_M4A1_SILENCER_STAT = 0
+skin_M4A1_SILENCER_WEAR = 0.0001F
+
+skin_M4A4 = 155
+skin_M4A4_STAT = 0
+skin_M4A4_WEAR = 0.0001F
+
+skin_MAC10 = 482
+skin_MAC10_STAT = 0
+skin_MAC10_WEAR = 0.0001F
+
+skin_MAG7 = 431
+skin_MAG7_STAT = 0
+skin_MAG7_WEAR = 0.0001F
+
+skin_MP7 = 482
+skin_MP7_STAT = 0
+skin_MP7_WEAR = 0.0001F
+
+skin_MP9 = 482
+skin_MP9_STAT = 0
+skin_MP9_WEAR = 0.0001F
+
+skin_NEGEV = 317
+skin_NEGEV_STAT = 0
+skin_NEGEV_WEAR = 0.0001F
+
+skin_NOVA = 286
+skin_NOVA_STAT = 0
+skin_NOVA_WEAR = 0.0001F
+
+skin_P2000 = 482
+skin_P2000_STAT = 0
+skin_P2000_WEAR = 0.0001F
+
+skin_P250 = 482
+skin_P250_STAT = 0
+skin_P250_WEAR = 0.0001F
+
+skin_P90 = 482
+skin_P90_STAT = 0
+skin_P90_WEAR = 0.0001F
+
+skin_PP_BIZON = 542
+skin_PP_BIZON_STAT = 0
+skin_PP_BIZON_WEAR = 0.0001F
+
+skin_R8_REVOLVER = 595
+skin_R8_REVOLVER_STAT = 0
+skin_R8_REVOLVER_WEAR = 0.0001F
+
+skin_SAWED_OFF = 256
+skin_SAWED_OFF_STAT = 0
+skin_SAWED_OFF_WEAR = 0.0001F
+
+skin_SCAR20 = 391
+skin_SCAR20_STAT = 0
+skin_SCAR20_WEAR = 0.0001F
+
+skin_SSG08 = 624
+skin_SSG08_STAT = 0
+skin_SSG08_WEAR = 0.0001F
+
+skin_SG556 = 287
+skin_SG556_STAT = 0
+skin_SG556_WEAR = 0.0001F
+
+skin_TEC9 = 287
+skin_TEC9_STAT = 0
+skin_TEC9_WEAR = 0.0001F
+
+skin_UMP45 = 556
+skin_UMP45_STAT = 0
+skin_UMP45_WEAR = 0.0001F
+
+skin_USP_SILENCER = 183
+skin_USP_SILENCER_STAT = 0
+skin_USP_SILENCER_WEAR = 0.0001F
+
+skin_XM1014 = 393
+skin_XM1014_STAT = 0
+skin_XM1014_WEAR = 0.0001F
